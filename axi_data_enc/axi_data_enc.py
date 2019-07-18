fd241b6d20feba55eedeadbbc6180dfb3ced4b25
Raghav Vaidyanathan Subbaraman <raghav1900@gmail.com>
2019-07-18 22:37:40 +0530

    some additions

--- axi_data_enc/axi_data_enc.py
+++ axi_data_enc/axi_data_enc.py
@@ -7,9 +7,11 @@
 # Author: Raghav Subbaraman
 # Organisation: IIT Madras
 # A python script that serialises a bunch of variables in a csv file
-# into AXI messages and vice-versa.
+# into AXI messages and vice-versa. It can also define structures or 
+# assign values to structure elements.
 # Changelog: 
 #	29 March 2019: File created
+#	17 July 2019: Some major changes
 ##################################################
 
 from optparse import OptionParser
@@ -20,44 +22,25 @@ import csv
 import pandas as pd
 import pdb
 
-def encode_axi_sub(options,df_in):
-	start_ind_vec = df_in['start_addr'].tolist()
-	var_name_vec = df_in['var_name'].tolist()
-	val_range_vec = df_in['val_range'].tolist()
-	desc_vec = df_in['desc'].tolist()
-	val_vec = df_in['valid'].tolist()
-	ip_var_vec = df_in['ip_var_name']
-	op_var_vec = df_in['op_var_name']
-	for i in range(options.prev_index,options.index):
-		description = str(desc_vec[i])
-		value_range = str(val_range_vec[i])
-		print("// "+description.strip()+', '+value_range.strip())
-		if(options.mode==0):
-			if(val_vec[i]==1):
-				print(op_var_vec[i]+'.range('+str(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w)+','+str(start_ind_vec[i]-options.cur_mesg*options.axi_w)+')'+' = '+ip_var_vec[i]+'.'+var_name_vec[i]+';')
-			else:
-				print(op_var_vec[i]+'.range('+str(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w)+','+str(start_ind_vec[i]-options.cur_mesg*options.axi_w)+')'+' = 0;')
-		else:
-			if(val_vec[i]==1):
-				print(ip_var_vec[i]+'.'+var_name_vec[i]+' = '+op_var_vec[i]+'.range('+str(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w)+','+str(start_ind_vec[i]-options.cur_mesg*options.axi_w)+')'+';')
-			else:
-				print(ip_var_vec[i]+'.'+var_name_vec[i]+' = 0;')
-
-	return 0
-
 def encode_axi_sub_2(options,df_in):
 	start_ind_vec = df_in['start_addr'].tolist()
 	start_ind_vec.insert(0,0)
-	var_name_vec = df_in['var_name'].tolist()
-	val_range_vec = df_in['val_range'].tolist()
-	desc_vec = df_in['desc'].tolist()
+
+	ind = 0;
+	for entry in start_ind_vec:
+		start_ind_vec[ind] = entry+options.offset;
+		ind = ind+1;
+
+	var_name_vec = df_in['Parameter'].tolist()
+	# val_range_vec = df_in['val_range'].tolist()
+	desc_vec = df_in['Description'].tolist()
 	val_vec = df_in['valid'].tolist()
-	ip_var_vec = df_in['ip_var_name']
+	ip_var_vec = df_in['struct_name']
 	op_var_vec = df_in['op_var_name']
 	for i in range(options.prev_index,options.index):
 		description = str(desc_vec[i])
-		value_range = str(val_range_vec[i])
-		print("// "+description.strip()+', '+value_range.strip())
+		# value_range = str(val_range_vec[i])
+		print("// "+description.strip())
 		if(options.mode==0):
 			if(val_vec[i]==1):
 				print(op_var_vec[i]+'.range('+str(int(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w))+','+str(int(start_ind_vec[i]-options.cur_mesg*options.axi_w))+')'+' = '+ip_var_vec[i]+'.'+var_name_vec[i]+';')
@@ -67,7 +50,7 @@ def encode_axi_sub_2(options,df_in):
 			if(val_vec[i]==1):
 				print(ip_var_vec[i]+'.'+var_name_vec[i]+' = '+op_var_vec[i]+'.range('+str(int(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w))+','+str(int(start_ind_vec[i]-options.cur_mesg*options.axi_w))+')'+';')
 			else:
-				print('//'+ip_var_vec[i]+'.'+var_name_vec[i]+' = '+op_var_vec[i]+'.range('+str(int(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w))+','+str(int(start_ind_vec[i]-options.cur_mesg*options.axi_w))+')'+';')
+				print('//'+str(ip_var_vec[i])+'.'+str(var_name_vec[i])+' = '+op_var_vec[i]+'.range('+str(int(start_ind_vec[i+1]-1-options.cur_mesg*options.axi_w))+','+str(int(start_ind_vec[i]-options.cur_mesg*options.axi_w))+')'+';')
 		elif(options.mode==2):
 			if(val_vec[i]==1):
 				print(ip_var_vec[i]+'.'+var_name_vec[i]+' = '+op_var_vec[i]+';')
@@ -76,45 +59,73 @@ def encode_axi_sub_2(options,df_in):
 
 	return 0
 
-def encode_axi(options,df_in):
+def encode_axi_2(options,df_in):
 	cur_mesg = 1
+	df_in['start_addr'] = df_in['Size'].cumsum()
 	start_ind_vec = df_in['start_addr'].tolist()
+	start_ind_vec.insert(0,0)
 	options.prev_index = 0;
+	options.offset = 0;
 	for i in start_ind_vec:
-		if(i > cur_mesg*options.axi_w-1):
-			options.cur_mesg = cur_mesg - 1
-			options.index = start_ind_vec.index(i);
-			encode_axi_sub(options,df_in)
+		if(i > cur_mesg*options.axi_w):
+			print('//#BEGIN#')
+			# print('case '+str(state_name)+'_'+str(cur_mesg-1)+':')
+			# print('if(!fapi_in.empty()){')
+			# print('din = fapi_in.read();')
+			options.cur_mesg = cur_mesg - 1;
+			options.index = start_ind_vec.index(i)-1;
+
+			encode_axi_sub_2(options,df_in)
+
+			options.offset = start_ind_vec[options.index] - cur_mesg*options.axi_w;
+
+			pdb.set_trace();
+
 			options.prev_index = options.index;
 			cur_mesg = cur_mesg+1
+			# print('CTRL_OUT.write(dout);')
+			# print('fapi_rx_sim_state = '+str(state_name)+'_'+str(cur_mesg-1)+';')
+			# # print('}')
+			print('//#END#')
 			print
 	print("Total Number of AXI Messages = "+str(cur_mesg))
 	return 0;
 
-def encode_axi_2(options,df_in):
-	cur_mesg = 1
-	df_in['start_addr'] = df_in['bit_size'].cumsum()
-	start_ind_vec = df_in['start_addr'].tolist()
-	start_ind_vec.insert(0,0)
-	options.prev_index = 0;
-	state_name = df_in['state_name'][0]
-	for i in start_ind_vec:
-		if(i > cur_mesg*options.axi_w-1):
-			print('case '+str(state_name)+'_'+str(cur_mesg-1)+':')
-			# print('if(!fapi_in.empty()){')
-			# print('din = fapi_in.read();')
-			options.cur_mesg = cur_mesg - 1
-			options.index = start_ind_vec.index(i);
-			encode_axi_sub_2(options,df_in)
-			options.prev_index = options.index;
-			cur_mesg = cur_mesg+1
-			print('CTRL_OUT.write(dout);')
-			print('fapi_rx_sim_state = '+str(state_name)+'_'+str(cur_mesg-1)+';')
-			# print('}')
-			print('break;')
-			print
-		
-	print("Total Number of AXI Messages = "+str(cur_mesg))
+def struct_encode(options,df_in):
+	# Function that creates structures from excel sheets
+	struct_name_vec = df_in['struct_name'].tolist()
+	parameter_vec = df_in['Parameter'].tolist()
+	size_vec = df_in['Size'].tolist()
+	desc_vec = df_in['Description'].tolist();
+
+	cur_struct = '';
+	idx = 0;
+	# pdb.set_trace();
+
+	for parameter in parameter_vec:
+		if((not isinstance(parameter, str)) or str(size_vec[idx])=='0'):
+			idx = idx + 1;
+			continue;
+
+		if(cur_struct != struct_name_vec[idx]):
+			print('}'+str(cur_struct)+';');
+			print();
+			cur_struct = struct_name_vec[idx];
+			print('typedef struct '+str(cur_struct)+'{');
+
+		if(size_vec[idx].isdigit()):
+			type_str = 'ap_uint<'+str(size_vec[idx])+'>';
+		else:
+			type_str = str(size_vec[idx]);
+
+		print('/*\n'+str(desc_vec[idx])+'\n*/');
+		print('    '+type_str+' '+str(parameter)+';');
+		# print('');
+		idx = idx + 1;
+
+	print('}'+str(cur_struct)+';');
+	cur_struct = struct_name_vec[idx-1];
+
 	return 0;
 
 def get_options():
@@ -125,7 +136,7 @@ def get_options():
 		help="Width of the AXI bus")
 	parser.add_option("-m", "--mode", type="int", dest="mode",
 		default=0,
-		help="Mode of variable conversion (0 - For var -> AXI | 1 - for AXI -> var)")
+		help='Mode of variable conversion \n 0 - For var -> AXI \n 1 - for AXI -> var \n 2 - Struct Assignment \n 3 - Struct Definition')
 	(options, filename) = parser.parse_args()
 	return (options, filename)
 
@@ -134,11 +145,15 @@ def main():
 	options.filename = filename;
 	df_in = pd.read_csv(options.filename[0])
 	df_out = pd.DataFrame()
-	# pdb.set_trace()
 	# encode_axi(options,df_in)
-	encode_axi_2(options,df_in)
+	if(options.mode == 3):
+		struct_encode(options,df_in);
+	else:
+		encode_axi_2(options,df_in)
 	# pdb.set_trace()
+	print('/*')
 	print(options)
+	print('*/')
 
 if __name__== "__main__":
 	main()
